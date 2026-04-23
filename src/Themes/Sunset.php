44930a80072a429df2745f22cfab484e79640e52
Tech-Loyal <103830225+Tech-Loyal@users.noreply.github.com>
2026-04-23 10:21:45 -0500

    Add getPublicPath method to Sunset theme

--- src/Themes/Sunset.php
+++ src/Themes/Sunset.php
@@ -19,6 +19,11 @@ class Sunset implements HasChangeableColor, Theme
         return __DIR__ . '/../../resources/dist/sunset.css';
     }
 
+    public static function getPublicPath(): string
+    {
+        return 'vendor/hasnayeen/themes/sunset.css';
+    }
+
     public function getThemeColor(): array
     {
         return Arr::except(Color::all(), ['gray', 'zinc', 'neutral', 'stone']);
